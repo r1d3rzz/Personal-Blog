027ec32028a18e9319f6ab8afbf3bf46d12ba434
r1d3rzz <myintthwaykhine127@gmail.com>
2022-10-29 15:51:33 +0630

    vue init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createApp(App).mount("#app");
